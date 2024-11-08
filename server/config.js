8f99508db69516021264cba056a64a93886f8a00
Milano3852 <denchikblinov2005@gmail.com>
2024-11-09 00:31:47 +0800

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
   maxThreads: parseInt(process.env.MAX_THREADS) || 5,
   speedLimit: parseInt(process.env.SPEED_LIMIT) || 500,  // скорость в КБ/сек
-  port: parseInt(process.env.PORT) || 3000
+  port: parseInt(process.env.PORT) || 10000
 };
